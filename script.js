233f55af1d939179bff716c2dbe8ab54e00ebfb1
Joanna Preston <joannappreston@gmail.com>
2020-04-06 15:15:36 -0700

    generate new city buttons and add event listener to them

--- script.js
+++ script.js
@@ -11,39 +11,31 @@
 
 var searchArr = [];
 var lastCity;
-var searchCity = "Seattle";
-var lat;
-var long;
+var searchCity = "Tampa";
 
 
 
-
-
-
-// * Create a function for ajax method and pulling data using open weather API
-//      - save needed data to variables: api Key, queryURL, date, icon, temp, humidity, windspeed, UV index
-//      - renders data pulled to the DOM
-//      - Needed data:
-//          + Current conditions: city, date, icon, temperature, humidity, windspeed, UV index (color coded)
-//          + 5 day forcast: date, icon, temperature, humidity
-
-
+// Defines a function for getting weather data
 function getWeatherData(searchCity) {
+    $("#forecast-cards").empty();
+    $("#forecast-header").empty();
+    $("#current-card").empty();
     
+    // get latitude and longitude coordinates using city name via opencage API
     var cordsURL = "https://api.opencagedata.com/geocode/v1/json?q=" + searchCity + "&key=bc3ff0db1a6d4ff49ac9914be9c0da3b&limit=1";
 
-    // get latitude and longitude coordinates using city name via opencage API
     $.ajax({
         url: cordsURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
 
-        lat = response.results[0].geometry.lat;
-        long = response.results[0].geometry.lng;
-        var weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&appid=5e347807f48ed981edea55456e7fea41"
+        // store lat and long in variables
+        var lat = response.results[0].geometry.lat;
+        var long = response.results[0].geometry.lng;
 
         // use lat and long points to get data from openweather API
+        var weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&appid=5e347807f48ed981edea55456e7fea41"
+
         $.ajax({
             url: weatherURL,
             method: "GET"
@@ -55,11 +47,11 @@ function getWeatherData(searchCity) {
 
             // save current weather data to variables
             var currentDate = moment.unix(current.dt).format('M/D/YYYY');
-            var currentTemp = Math.round((current.temp * (9 / 5)) - 459.67);
+            var currentTemp = Math.round((current.temp * (9 / 5)) - 459.67) + "&#176;";
             var windSpeedMPH = (current.wind_speed * 2.23694).toFixed(1);
             var currentIconURL = "http://openweathermap.org/img/wn/" + current.weather[0].icon + ".png";
             var currentUV = current.uvi;
-            var currentHum = current.humidity;
+            var currentHum = current.humidity + "%";
 
             // generate current card based on current weather variables
             var currentCard = $('<div class="col"><div class="card"><div class="card-body"><h2>' + searchCity + " (" + currentDate + ")" + '<img id="current-icon" src=' + currentIconURL +'></h2><div id="current-info"><p>Temperature: ' + currentTemp + '</p><p>Humidity: ' + currentHum + '</p><p>Windspeed: ' + windSpeedMPH + '</p><p>UV Index: ' + currentUV + '</p></div></div></div></div >')
@@ -72,23 +64,23 @@ function getWeatherData(searchCity) {
             // ****** 5-DAY FORECAST ******** //
             var daily = response.daily
 
+            // generate and append forecast header to the DOM
             var forecastHeader = $('<h4>5-Day Forecast:</h4>');
             $("#forecast-header").append(forecastHeader);
 
 
-            // pull daily data for 5 days using for loop, save to variables and render to the DOM using newly generated cards
+            // loops through 5 days of daily forcast data
             for (var i = 1; i < 6; i++) {
                 
+                // saves data to variables
                 var dailyDate = moment.unix(daily[i].dt).format('M/D/YYYY');
                 var dailyTemp = Math.round((daily[i].temp.max * (9 / 5)) - 459.67) + "&#176;";
                 var dailyHum = daily[i].humidity + "%";
                 var dailyIconURL = "http://openweathermap.org/img/wn/" + daily[i].weather[0].icon + ".png";
 
+                // generate new daily forecast card using variables
+                var dailyCard = $('<div class="col forecast-card"><div class= "card text-white bg-primary"><div class="card-body"><h5 class="card-title">' + dailyDate + '</h5><img class="forecast-icon" src=' + dailyIconURL + '><p class="card-text">Temp: ' + dailyTemp + '</p><p class="card-text">Humidity: '+ dailyHum + '</p></div></div></div>');
 
-                // generate new daily forecast card using daily data
-                var dailyCard = $('<div class="col"><div class= "card text-white bg-primary"><div class="card-body"><h5 class="card-title">' + dailyDate + '</h5><img class="forecast-icon" src=' + dailyIconURL + '><p class="card-text">Temp: ' + dailyTemp + '</p><p class="card-text">Humidity: '+ dailyHum + '</p></div></div></div>');
-
-                dailyCard.addClass("forecast-card");
 
                 // append new daily card to forecast cards div
                 $("#forecast-cards").append(dailyCard);
@@ -97,18 +89,41 @@ function getWeatherData(searchCity) {
     })
 };
 
-getWeatherData(searchCity);
-
-
-
 
 // * Create event listener for when user types in a new city to search bar and clicks submit 
 //      - changes searchCity variable
 //      - calls ajax function to pull data from API and render to DOM
 //      - adds city to search history (creates new item to click on)
 //      - pushes new city to local storage (call push local storage function)
-// * Create event listener for when a user clicks on a city from search history
-//      - calls ajax function to pull data from API and render to DOM
+
+$(document).ready(function () {
+
+    $("#search-btn").on("click", function(event) {
+        // event.preventDefault();
+        searchCity = $("#search-input").val();
+
+        getWeatherData(searchCity);
+        genCityButton();
+    });
+
+    // * Create event listener for when a user clicks on a city from search history
+    //      - calls ajax function to pull data from API and render to DOM
+    $(document).on("click", ".city-btn", function(event) {
+        // event.preventDefault();
+        searchCity = $(this).text();
+        getWeatherData(searchCity);
+    });
+
+});
+
+// create funciton to generate city button when user searches for a city
+function genCityButton() {
+    var cityBtn = $('<button type="button" class="list-group-item list-group-item-action city-btn"></button>')
+    cityBtn.text(searchCity);
+    $("#city-list").append(cityBtn);
+}
+
+
 // * Create a funciton to store data in local storage
 //      - store search history
 //      - store last searched city
